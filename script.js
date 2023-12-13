03a2092dd95e3d8702de45eccffae2fb6b1580aa
yash7xm <yash.7xm@gmail.com>
2023-12-13 14:52:35 +0530

    block head added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const Rows = document.querySelector(".rows");
 const Cols = document.querySelector(".columns");
 const btn = document.querySelector(".btn");
+const bName = document.querySelector(".block-name");
 
 var canvas = new fabric.Canvas("canvas", {
   top: 50,
@@ -15,15 +16,26 @@ let seatNums = [];
 btn.addEventListener("click", () => {
   const rows = Rows.value;
   const cols = Cols.value;
+  const blockName = bName.value;
 
   canvas.clear();
   seatNums = [];
+
+  const blockHeading = new fabric.Text(`${blockName}`, {
+    left: 1.5 * seatSize,
+    top: 0,
+    fontSize: 25,
+    fill: "grey",
+    selectable: false,
+  });
+
+  canvas.add(blockHeading);
 
   for (let i = 0; i < rows; i++) {
     const letter = String.fromCharCode(65 + i);
     const rowName = new fabric.Text(`${letter}`, {
       left: 0,
-      top: i * seatSize * 2 + seatSize / 2.5,
+      top: i * seatSize * 2 + seatSize / 2.5 + 25,
       fontSize: 25,
       fill: "grey",
       selectable: false,
@@ -33,7 +45,7 @@ btn.addEventListener("click", () => {
 
     for (let j = 0; j < cols; j++) {
       const left = j * seatSize * 2 + seatSize;
-      const top = i * seatSize * 2;
+      const top = i * seatSize * 2 + 25;
 
       const seat = new fabric.Circle({
         left: left,
@@ -56,6 +68,11 @@ btn.addEventListener("click", () => {
       });
 
       seat.on("mousedown", function () {
+        toggleSeatSelection(seat);
+        modifySeatNums(seatNum.seatNumber, cols);
+      });
+
+      seatNum.on("mousedown", function () {
         toggleSeatSelection(seat);
         modifySeatNums(seatNum.seatNumber, cols);
       });
