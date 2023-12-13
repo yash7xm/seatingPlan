ad43a08d126c7f162c50723c5404667f92f90641
yash7xm <yash.7xm@gmail.com>
2023-12-13 17:44:28 +0530

    file structure updated

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+import a from './utils/objectClass.js';
+import modifySeatNums from './utils/editSeats.js';
+
 const Rows = document.querySelector(".rows");
 const Cols = document.querySelector(".columns");
 const btn = document.querySelector(".btn");
 const bName = document.querySelector(".block-name");
 const seatEditing = document.querySelector(".activate-seat-editing-btn button");
 const clearSeatsBtn = document.querySelector(".clear-seats button");
+
 
 var canvas = new fabric.Canvas("canvas", {
   top: 50,
@@ -78,18 +82,18 @@ btn.addEventListener("click", () => {
       });
 
       seat.on("mousedown", function () {
-        toggleSeatSelection(seat);
-        modifySeatNums(seatNum.seatNumber, cols);
+        toggleSeatSelection(seat, seatNum);
+        modifySeatNums(seatNum.seatNumber, cols, seatNums);
       });
 
       seat.on("click", function () {
-        toggleSeatSelection(seat);
-        modifySeatNums(seatNum.seatNumber, cols);
+        toggleSeatSelection(seat, seatNum);
+        modifySeatNums(seatNum.seatNumber, cols, seatNums);
       });
 
       seatNum.on("mousedown", function () {
-        toggleSeatSelection(seat);
-        modifySeatNums(seatNum.seatNumber, cols);
+        toggleSeatSelection(seat, seatNum);
+        modifySeatNums(seatNum.seatNumber, cols, seatNums);
       });
 
       canvas.add(seat);
@@ -132,59 +136,25 @@ clearSeatsBtn.addEventListener("click", () => {
   canvas.renderAll();
 });
 
-function toggleSeatSelection(seat) {
+function toggleSeatSelection(seat, seatNum) {
   const currentFill = seat.get("fill");
   seat.set("fill", currentFill === "blue" ? "grey" : "blue");
 
   if (currentFill === "grey") {
     const index = clearSeats.indexOf(seat);
+    const seatNumIndex = clearSeats.indexOf(seatNum);
     if (index !== -1) {
       clearSeats.splice(index, 1);
     }
+    if (seatNumIndex !== -1) {
+      clearSeats.splice(seatNumIndex , 1);
+    }
+
   } else {
     clearSeats.push(seat);
+    clearSeats.push(seatNum);
   }
 
   canvas.renderAll();
 }
 
-function modifySeatNums(seatNumber, cols) {
-  if (seatNums[seatNumber - 1].text === "") addSeats(seatNumber, cols);
-  else removeSeats(seatNumber, cols);
-}
-
-function removeSeats(seatNumber, cols) {
-  seatNumber = parseInt(seatNumber);
-  cols = parseInt(cols);
-  let start = seatNumber % cols;
-  if (start === 0) start = cols;
-  let j = seatNumber;
-  let no = seatNums[seatNumber - 1].text;
-  for (let i = start; i < cols; i++, j++) {
-    if (seatNums[j].text === "") continue;
-    else {
-      seatNums[j].text = `${no}`;
-      no++;
-    }
-  }
-
-  seatNums[seatNumber - 1].text = "";
-}
-
-function addSeats(seatNumber, cols) {
-  seatNumber = parseInt(seatNumber);
-  cols = parseInt(cols);
-  let start = seatNumber % cols;
-  if (start == 0) start = cols;
-  let no = 1;
-  let j = seatNumber - start;
-
-  for (let i = 0; i < cols; i++, j++) {
-    if (seatNums[j].text === "" && i != start - 1) {
-      continue;
-    } else {
-      seatNums[j].text = `${no}`;
-      no++;
-    }
-  }
-}
